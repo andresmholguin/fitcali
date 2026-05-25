d0d7cf75c48df4f7f7659e0bc7cedfcc55104986
Andres Holguin Work <andresmholguin@gmail.com>
2026-05-25 12:12:03 -0500

    Nav volver a franjas

--- src/pages/FranjaPage.jsx
+++ src/pages/FranjaPage.jsx
@@ -24,8 +24,29 @@ export default function FranjaPage() {
       <Navbar />
 
       <div id="wrapper-frame">
-        <div className="container">
-          <h1 className="text-center mb-5">{franja}</h1>
+        <div className="container ">
+          <nav className="d-flex flex-row justify-content-center align-items-center position-relative  mb-5">
+            <a
+              href="/fitcali.colboletos.com"
+              className="btn btn-light position-absolute start-0 gap-2 d-flex flex-row align-items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chevron-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                />
+              </svg>
+              Franjas
+            </a>
+            <h1 className="text-center">{franja}</h1>
+          </nav>
 
           <div className="row justify-content-center">
             {eventosFranja.map((evento, index) => (
